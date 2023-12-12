71a3cb303eda155ad396d2c033d9a74149039114
Hakimov <naim.nanocoding@gmail.com>
2023-12-12 10:47:51 +0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
-import { Bot } from "grammy";
+import { Bot, webhookCallback } from "grammy";
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
 
@@ -45,4 +46,17 @@ bot.on('message', async (ctx) => {
     }
 });
 
-bot.start();
+if (process.env.NODE_ENV === "production") {
+    // Use Webhooks for the production server
+    const app = express();
+    app.use(express.json());
+    app.use(webhookCallback(bot, "express"));
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Bot listening on port ${PORT}`);
+    });
+} else {
+    // Use Long Polling for development
+    bot.start();
+}
